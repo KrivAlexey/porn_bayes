2a34fce7645dd90f0f3d23ee5051df79d0757db3
Krivykh Alexey <krivykhalexey@gmail.com>
2012-01-10 03:29:34 +0400

    научил его учиться)

--- porn_bayes.pl
+++ porn_bayes.pl
@@ -2,57 +2,93 @@
 
 use LWP::UserAgent;
 use HTML::Parser;
+use YAML::Tiny qw 'DumpFile LoadFile';
 use utf8;
+use List::MoreUtils qw 'uniq';
 use strict;
 use open qw(:std :utf8);
+use Encode;
 
-my $ua = LWP::UserAgent->new;
-my $content =  $ua->get("http://gigporno.ru/")->content;
-utf8::decode($content);
-
+#Флаг для обучения, 
 my $file4content = "content.txt";
-open X, ">", $file4content
-or die  "$0 : failed to open  input file '$file4content' : $!\n";
-print X $content;
-close X
-or warn or die  "$0 : failed to close input file '$file4content' : $!\n";
+my @bad_urls = qw(http://gigporno.ru/ http://www.trahun.tv/ http://xrest.net/ http://www.mega-porno.ru/ http://www.dojki.com/ http://www.bigsexshok.ru/ 
+http://www.porno4all.info/ http://www.5xxx.ru/ http://arhiv-porno.com/ http://www.5porno.ru/ http://www.porn.com/ http://www.pornhub.com/ 
+http://www.xnxx.com/);
+#my $urls =  'http://www.trahun.tv/';
+my @good_urls = qw(http://www.zoovet.ru/ http://www.dikiezveri.com/ http://www.ikea.com/ http://ekologiya.net/ http://www.ecology.com/ http://help.com/ 
+http://www.polit.ru/ http://www.bz.ru/ http://www.spbu.ru/structure/ http://zhitomir.info/ http://www.boston.com/ http://www.gismeteo.ru/ 
+http://www.b177.ru/);
 
-my $p = HTML::Parser->new(api_version => 3, text_h =>[\&x, "dtext"]);
+my @words;
+    my $p = HTML::Parser->new(api_version => 3, text_h=>[ \&x, "text"]);
+     
+    $p->ignore_elements(qw(script style));
+    $p->unbroken_text(1);
+    $p->utf8_mode(1);
+    $p->attr_encoded(1);
+   
+    sub x {
+        my  ($qq) = @_;
+        utf8::decode($qq);
+        $qq = lc($qq);
+        @words =  $qq=~ /([a-zа-я]{3,})/g;
+    }
 
-$p->ignore_elements(qw(script style));
-$p->unbroken_text(1);
-$p->utf8_mode(1);
-$p->attr_encoded(1);
+learn_filter ( 1, @bad_urls);
 
-$p-> parse_file($file4content) || die $!;
+learn_filter ( 0, @good_urls);
 
-sub x {
-    my ($qq) = @_;
-    utf8::decode($qq);
-    $qq = lc($qq);
-    my @a = $qq=~ /([a-zа-я]{3,})/g;
-    make_stat(@a);
-}
+#учим фильтр\
+sub learn_filter{
+    my ($porn_flag, @urls) = @_;
     
+    foreach (@urls){   
+        get_content($_);
+        $p-> parse_file($file4content) ;
+        make_stat($porn_flag, @words);
+    }
+
+}
+
+#достаем контент страницы
+sub get_content{
+    my ($url) = @_;
+    my $encoding = 'utf-8';
+    my $ua = LWP::UserAgent->new;
+    my $content =  $ua->get($url)->content ;
+    $content =~ /charset=\"?(.*?)\"/;
+    print "$url\n";
+    $encoding = $1 unless $1=='';
+    $content = decode($encoding, $content);
+    
+    open X, ">", $file4content
+    or die  "$0 : failed to open  input file '$file4content' : $!\n";
+    print X $content;
+    close X
+    or warn "$0 : failed to close input file '$file4content' : $!\n";
+}
+
+#создаем статистику
 sub make_stat {
-    my @words = @_;
-    open FH, ">", "porn.txt";
-    print FH join "\n", @words;
-    my $file_name = "stat.txt";
+    my ($pornFlag, @words) = @_;
+    print "$pornFlag \n";
+    my $file_name = 'stat.yml';
+    my %stat;
+    my $quantityOfpages = 'quantityOfpages';
+
+    @words = uniq @words;    
+    #map{$_/=scalar @words}values %stat;
+
     if (-e $file_name){
-       open my $fh, "<", $file_name;
-       #@oldStat = 
+        my $old_stat = LoadFile($file_name);
+        foreach (@words){
+           $pornFlag ? $$old_stat{$_}++ :$$old_stat{$_}--; 
+        }
+        $$old_stat{$quantityOfpages} ++;
+        DumpFile($file_name, $old_stat)or die "ololo: $!" ;
     }
     else { 
-       my %stat;
-       foreach (@words) {
-           $stat{$_} ++;   
-       }
-       #map{$_/=scalar @words}values %stat;
-       open my $fh, ">", $file_name
-       or die "$0 :faild to open input file $file_name : $!\n";
-       print $fh join " ", %stat;
-       close $fh;
+        $stat{$quantityOfpages} = 1;
+        DumpFile($file_name, \%stat)or die "ololo: $!" ;
    }
-
 } ## --- end sub make_stat
